d43db48b07c6dfe02c5f9cb9dfa48009e94fcb69
gucluakkaya <gucluakkaya@d0bf2403-168d-da78-1e31-d8e4dff4f114>
2012-11-20 23:07:52 +0000

    Files added to a local directory instead of db. Only relative path of files are stored

--- service/src/main/java/com/swe/accessibility/controller/EntryController.java
+++ service/src/main/java/com/swe/accessibility/controller/EntryController.java
@@ -1,22 +1,33 @@
 package com.swe.accessibility.controller;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.security.Principal;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,7 +40,6 @@ import com.swe.accessibility.domain.InsertEntryResult;
 import com.swe.accessibility.domain.SubReason;
 import com.swe.accessibility.domain.User;
 import com.swe.accessibility.domain.proxy.EntryList;
-import com.swe.accessibility.domain.proxy.EntryProxy;
 import com.swe.accessibility.domain.proxy.ThumbsObject;
 import com.swe.accessibility.domain.proxy.UpdateResult;
 import com.swe.accessibilty.service.EntryService;
@@ -40,6 +50,9 @@ import com.swe.accessibilty.service.UserService;
 @RequestMapping("/entries")
 public class EntryController {
 
+	//Assuming it is a linux/unix file system
+	private static String uploadRepo = System.getProperty("user.home") + File.separator + "uploads";
+	
 	@Resource(name = "entryService")
 	private EntryService entryService;
 	
@@ -48,6 +61,8 @@ public class EntryController {
 
 	@Resource(name = "userService")
 	private UserService userService;
+	
+	
 	
 //	@RequestMapping(value = "/add", method = RequestMethod.POST, headers={"content-type=multipart/form-data","Access-Control-Allow-Origin:*"},produces={"application/json"})
 //	public @ResponseBody InsertEntryResult  insertEntry(MultipartHttpServletRequest request,
@@ -110,6 +125,8 @@ public class EntryController {
 //
 //	}
 	
+	
+
 	@RequestMapping(value = "/add", method = RequestMethod.POST, headers={"content-type=multipart/form-data"},produces={"application/json"})
 	public  ResponseEntity<InsertEntryResult>  insertEntry(MultipartHttpServletRequest request,
 			HttpSession session, Principal principal) {
@@ -121,7 +138,7 @@ public class EntryController {
 		BigDecimal coordX;
 		BigDecimal coordY;
 		
-		MultipartFile file = request.getFile("file");
+		
 		String username = null;
 		if (principal == null)
 		
@@ -130,7 +147,7 @@ public class EntryController {
 			username = principal.getName();
 		try {
 			coordX = (BigDecimal) format.parse(request.getParameter("coordX"));
-			coordY = (BigDecimal) format.parse(request.getParameter("coordX"));
+			coordY = (BigDecimal) format.parse(request.getParameter("coordY"));
 			
 			User currentUser = userService.getUserByName(username);
 			String comment = request.getParameter("comment");
@@ -141,11 +158,12 @@ public class EntryController {
 			
 			reasons.add(reason);
 			
+			String uri = saveFile(request);
 			entry.setComment(comment);
 			entry.setCoordX(coordX);
 			entry.setCoordY(coordY);
 			entry.setReasons(reasons);
-			entry.setImageMeta(file.getBytes());
+			entry.setImageMeta(uri);
 			entry.setUser(currentUser);
 			
 			
@@ -162,8 +180,8 @@ public class EntryController {
 			entryResult.setResultId(2);
 			
 		} catch (IOException e) {
-			
-			entryResult.setResultStatus("Not a valid file");
+			e.printStackTrace();
+			entryResult.setResultStatus("Error saving file");
 			entryResult.setResultId(3);
 		}
 		
@@ -193,6 +211,34 @@ public class EntryController {
 //		return entity;
 //	}
 	
+	private String saveFile(MultipartHttpServletRequest request) throws IOException {
+		
+		MultipartFile file = request.getFile("file");
+		
+		InputStream in = new ByteArrayInputStream(file.getBytes());
+		ImageInputStream iis = ImageIO.createImageInputStream(in);
+		Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+		String format = null;
+		 while (readers.hasNext()) {
+		        ImageReader read = readers.next();
+		        format = read.getFormatName();
+		        System.out.println("format name = " + format);
+		         
+		 }
+		BufferedImage image = ImageIO.read(iis);
+		
+		int scaledWidth = image.getHeight() / 4;
+		int scaledHeight = image.getWidth() / 4;
+		
+		BufferedImage resizedImage = createResizedCopy(image, scaledWidth, scaledHeight, true);
+		String fileURL = UUID.randomUUID().toString() + "." + format;
+		
+		File output = new File(uploadRepo + File.separator + fileURL);
+		FileOutputStream os = new FileOutputStream(output);
+		ImageIO.write(resizedImage, format, os);
+		return fileURL;
+	}
+
 	@RequestMapping(method={RequestMethod.GET},produces={"application/json"})
 	public  ResponseEntity<EntryList> getEntry(@RequestParam(required=false) String x, @RequestParam(required=false) String y,@RequestParam(required=false) String categoryId,@RequestParam(required=false) String typeId){
 		
@@ -217,6 +263,8 @@ public class EntryController {
 				result = getEntryByCategory(categoryId);
 			else if(typeId != null)
 				result = getByType(typeId);
+			else
+				result = getEntries();
 		}catch(ParseException e){
 			
 		}
@@ -299,6 +347,14 @@ public class EntryController {
 	
 		return result;
 	}
+	
+	private EntryList getEntries(){
+		
+		EntryList result = new EntryList();
+		result.setData(entryService.loadEntries());
+		
+		return result;
+	}
 
 //	@RequestMapping(value="/get",method={RequestMethod.GET},produces={"application/json"},headers={"Access-Control-Allow-Origin:*"})
 //	public @ResponseBody EntryList getEntry(@RequestParam String x, @RequestParam String y){
@@ -330,5 +386,21 @@ public class EntryController {
 		
 		return responseHeaders;
 	}
+	
+	private static BufferedImage createResizedCopy(Image originalImage, 
+    		int scaledWidth, int scaledHeight, 
+    		boolean preserveAlpha)
+    {
+    	
+    	int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+    	BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+    	Graphics2D g = scaledBI.createGraphics();
+    	if (preserveAlpha) {
+    		g.setComposite(AlphaComposite.Src);
+    	}
+    	g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
+    	g.dispose();
+    	return scaledBI;
+    }
 	
 }
