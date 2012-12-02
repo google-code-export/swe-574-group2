aa80a2da19d5130765ff574e595f66d65f370404
akazaz11@gmail.com <akazaz11@gmail.com@d0bf2403-168d-da78-1e31-d8e4dff4f114>
2012-12-02 19:29:02 +0000

    Login UI changed for new web service

--- trunk/mobile/FiarUrbanMobile/src/com/swe/fairurban/UI/Login.java
+++ trunk/mobile/FiarUrbanMobile/src/com/swe/fairurban/UI/Login.java
@@ -1,10 +1,16 @@
 package com.swe.fairurban.UI;
 
+import org.json.JSONObject;
+
 import com.swe.fairurban.R;
+import com.swe.fairurban.Helpers.OnServiceConnectionFinishedEvent;
+import com.swe.fairurban.Helpers.ServiceConnector;
+import com.swe.fairurban.Helpers.ServiceHelper;
 import com.swe.fairurban.Helpers.UserHelper;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
@@ -20,6 +26,8 @@ public class Login extends Activity {
 	EditText txtPassword;
 	Button btnLogin;
 	Context appContext;
+	ProgressDialog pd;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,23 +37,92 @@ public class Login extends Activity {
 		txtPassword = (EditText) findViewById(R.id.txtPassword);
 		btnLogin = (Button) findViewById(R.id.btnLogin);
 
+		txtUsername.setText(UserHelper.Username);
+		txtPassword.setText(UserHelper.Password);
+		
+		
+		pd = new ProgressDialog(Login.this);
+		
 		btnLogin.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View view) {
-				// TODO Auto-generated method stub
+				
+				runOnUiThread(new Runnable() {
+					
+					public void run() {
+				
+						pd.setMessage("Sunucuyla iletiþime geçiliyor.");
+						pd.setCancelable(false);
+						pd.show();
+					}
+				});
+				
+				
+				
 				String username = txtUsername.getText().toString();
 				String password = txtPassword.getText().toString();
 				
-				if (username.equalsIgnoreCase(UserHelper.Username) && password.equalsIgnoreCase(UserHelper.Password)) {
-					//Login successful
-					Intent mapIntent = new Intent(view.getContext(), Map.class);
-	                startActivity(mapIntent);
-	              
-				}
-				else {
-					Toast tLoginError = Toast.makeText(appContext, "Kullanýcý Adý / Þifre Hatalý",1000);
-					tLoginError.show();
-				}
+				ServiceConnector sc = new ServiceConnector(ServiceHelper.GetLoginUrl());
+				
+				sc.AddParameter("username", username);
+				sc.AddParameter("password", password);
+				
+				sc.AddListener(new OnServiceConnectionFinishedEvent() {
+					
+					@Override
+					public void ExceptionOccured() {
+					
+						runOnUiThread(new Runnable() {
+							public void run() {
+								pd.dismiss();
+								Toast tConnectionException = Toast.makeText(appContext, "Sunucuyla baðlantý kurulamadý.",1000);
+								tConnectionException.show();
+							}
+						});
+					}
+					
+					@Override
+					public void ConnectionFinished(final String result) {
+						
+						
+							
+							runOnUiThread(new Runnable() {
+								public void run() {
+									try {
+										pd.setMessage("Sunucudan gelen cevap iþleniyor.");
+										
+										JSONObject jObj = new JSONObject(result);
+										
+										if (jObj.getBoolean("loggedIn")) {
+											//Login successful
+											Intent mapIntent = new Intent(Login.this, Map.class);
+							                startActivity(mapIntent);
+										}
+										else {
+											Toast tLoginError = Toast.makeText(appContext, "Kullanýcý Adý / Þifre Hatalý",1000);
+											tLoginError.show();
+										}
+										
+										pd.dismiss();
+									} catch (Exception e) {
+										Toast tLoginError = Toast.makeText(appContext, "Beklenmeyen bir hata oluþtu.",1000);
+										tLoginError.show();
+										
+										pd.dismiss();
+									}
+								}
+							});
+							
+							
+						
+						
+					}
+				});
+				
+				sc.Connect();
+				
+				pd.setMessage("Baðlantý kuruluyor.");
+				
 			}
 		});
     }
