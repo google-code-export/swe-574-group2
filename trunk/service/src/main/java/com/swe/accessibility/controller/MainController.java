057aac5bd7b6c174e2dbb0ddcb7c876e690f205f
gucluakkaya <gucluakkaya@d0bf2403-168d-da78-1e31-d8e4dff4f114>
2012-12-09 22:43:11 +0000

    Exception handling redirecting to error pages added

--- trunk/service/src/main/java/com/swe/accessibility/controller/MainController.java
+++ trunk/service/src/main/java/com/swe/accessibility/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -117,4 +118,9 @@ public class MainController {
 //		return result;
 //	}
 //	
+	@ExceptionHandler(value=Exception.class)
+	public String handleException(){
+			
+		return "error";
+	}
 }
